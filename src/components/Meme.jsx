d1e963b3ca2efa8d88170d4ca7b9fd5d3fa7385a
Craig Neff <ifly12610@gmail.com>
2021-12-07 23:21:44 -0700

    working with state and local data

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme() {
+  const [memeImage, setMemeImage] = React.useState("");
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randNumb = Math.floor(Math.random() * memesArray.length);
+    console.log(memesArray[randNumb].url);
+    setMemeImage(memesArray[randNumb].url);
+  }
   return (
     <main>
       <div className="menu_form">
-        <input className="menu_input" placeholder="Top Text" type="text" />
-        <input className="menu_input" placeholder="Bottom Text" type="text" />
-        <button>Get a new meme image 😹</button>
+        <input className="menu_input_top" placeholder="Top Text" type="text" />
+        <input
+          className="menu_input_bottom"
+          placeholder="Bottom Text"
+          type="text"
+        />
+        <button onClick={getMemeImage}>Get a new meme image 😹</button>
       </div>
+      <img src={memeImage} />
     </main>
   );
 }
