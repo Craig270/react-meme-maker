d426f1c7abfc34e7acb5f82456c2769ed0501219
Craig Neff <ifly12610@gmail.com>
2021-12-09 23:16:47 -0700

    research for setting up changable text to be written over the top and bottom

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import memesData from "../memesData";
 
 export default function Meme() {
+  //Setting up the starting image when the page loads
   const [meme, setMeme] = React.useState({
     topText: "#",
     bottomText: "#",
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
   const [allMemeImages, setAllMemeImages] = React.useState(memesData);
-
+  //Function used to get random image while including all prvious data from the old object, and changing the desired changes in the second parameter
   function getMemeImage() {
     const memesArray = allMemeImages.data.memes;
     const randNumb = Math.floor(Math.random() * memesArray.length);
